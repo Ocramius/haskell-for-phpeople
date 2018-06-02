5cfee38e57ac40a9592e8dae1e4247377f94cada
Marco Pivetta <ocramius@gmail.com>
2018-06-02 23:52:01 +0200

    Removed unused slides

--- presentation/index.js
+++ presentation/index.js
@@ -44,14 +44,6 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["none"]} transitionDuration={0} theme={theme} controls={false}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
         <CodeSlide
           lang="php"
           code={require("raw-loader!./code-examples/primitive/register-globals.php")}
@@ -86,30 +78,6 @@ export default class Presentation extends React.Component {
             { loc: [9, 10], note: "This is fine" }
           ]}
         />
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
       </Deck>
     );
   }
